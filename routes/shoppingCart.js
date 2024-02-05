533be1da199ffebfeb06ad57471b10acb02292d2
Brandon Armstrong <barmstrong1188@gmail.com>
2024-02-05 18:40:12 -0500

    initial commit - post function to add book to shopping cart

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -1,1 +1,42 @@
 // Author: Brandon Armstrong
+import express from "express";
+import { db } from "../database.js";
+
+const router = express.Router();
+
+//************************************* */
+// Add a book to the shopping cart
+//************************************* */
+router.post("/cart/add", async (req, res) => {
+    try {
+        const { bookId, userId } = req.body;
+
+        if (!bookId || !userId) {
+            res.status(400).json({ error: "Book Id or User Id is missing in the request." });
+            return;
+        }
+
+        // Implement logic to add the book to the user's cart
+        // Update the user's cart in the database
+        await db.collection("carts").updateOne({ userId }, { $push: { books: bookId } });
+
+        res.status(200).json({ message: "Book added to the cart successfully." });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error. Please try again later." });
+    }
+});
+//************************************* */
+//Retrieve the list of books in the user's shopping cart
+//************************************* */
+//************************************* */
+//Delete a book from the shopping cart instance for that user
+//************************************* */
+//************************************* */
+//Helper function to calculate subtotal
+//************************************* */
+//************************************* */
+// Retrieve the subtotal price of all items in the user’s shopping cart.
+//************************************* */
+
+export default router;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import browsingRoutes from "./routes/bookBrowsing.js";
+import shoppingCartRoutes from "./routes/shoppingCart.js";
 import { connectMongoDB } from "./database.js";
 
 const app = express();
 app.use(express.json());
 
 connectMongoDB();
-
+//**ADD YOUR ROUTES HERE**/
 app.use(browsingRoutes);
-
+app.use(shoppingCartRoutes);
 app.listen(3000, () => {
     console.log("app listening on port 3000");
 });
