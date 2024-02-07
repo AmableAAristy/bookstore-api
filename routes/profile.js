ed2491cf614197eb2773f0d5b541006abf1766b6
Amathost <124377028+Amathost@users.noreply.github.com>
2024-02-07 17:44:54 -0400

    created post to create a new user

--- routes/profile.js
+++ routes/profile.js
@@ -1,1 +1,44 @@
 // Author: Amable Aristy
+
+import express from "express";
+import { db } from "../db.js";
+
+
+
+const router = express.Router();
+router.use(express.json())
+
+
+router.post('/users', (req, res) => {
+    const { username, password, realName, email, address } = req.body;
+    if (!username || !password) { //it needs both fields
+        return res.status(400).json({
+            error: "Username and password are required!"
+        });
+    }
+    
+    if (username && password) {
+        const newUser = { username, password };
+        if (realName) newUser.realName = realName;
+        if (email) newUser.email = email;
+        if (address) newUser.address = address;
+
+
+        db.collection('users')
+            .insertOne(newUser)
+            .then(result => {
+                res.status(201).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ err: 'Could not create a new user' })
+            })
+    }
+})
+
+router.get('/', (_, res) => {
+    res.send('I just want something to show. I am in profile js you can delete me if you want');
+});
+
+
+
+export default router;
