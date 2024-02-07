d23dd4d442b2173e14a03a44f75c681bad9dda2c
Javier Garcia <javierdangarcia@gmail.com>
2024-02-06 21:40:05 -0500

    fixed mongodb connection env variable

--- db.js
+++ db.js
@@ -3,13 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.nfrnv7j.mongodb.net/?retryWrites=true&w=majority`;
-
 let db;
 
 const connectToDatabase = async () => {
     try {
-        const client = await MongoClient.connect(uri);
+        const client = await MongoClient.connect(process.env.MONGODB_URI);
         db = client.db();
     } catch (error) {
         console.error(error);
