94060c67fb96264853784aa777acf1d7e17a72cd
Amathost <124377028+Amathost@users.noreply.github.com>
2024-02-07 19:13:01 -0400

    switched formatting for consistency with other branches

--- routes/profile.js
+++ routes/profile.js
@@ -9,31 +9,32 @@ const router = express.Router();
 router.use(express.json())
 
 
-router.post('/users', (req, res) => {
+router.post('/users', async (req, res) => {
     const { username, password, realName, email, address } = req.body;
     if (!username || !password) { //it needs both fields
         return res.status(400).json({
             error: "Username and password are required!"
         });
     }
-    
-    if (username && password) {
+    try {
         const newUser = { username, password };
         if (realName) newUser.realName = realName;
         if (email) newUser.email = email;
         if (address) newUser.address = address;
 
-
-        db.collection('users')
-            .insertOne(newUser)
-            .then(result => {
-                res.status(201).json(result)
-            })
-            .catch(err => {
-                res.status(500).json({ err: 'Could not create a new user' })
-            })
+        const result = await db.collection('users').insertOne(newUser);
+        const insertedId = result.insertedId
+        const response = {
+            message: "User created successfully",
+            insertedId: insertedId
+        };
+        res.status(201).json(response);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Could not create a new user' });
     }
-})
+}
+)
 
 router.get('/', (_, res) => {
     res.send('I just want something to show. I am in profile js you can delete me if you want');
