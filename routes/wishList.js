200de3e79806838a608b55c35053ef29a638e96e
ralooo <157410975+ralooo@users.noreply.github.com>
2024-03-26 21:27:48 -0400

    Update wishList.js
    inputting a wishlist into the database and modifying it

--- routes/wishList.js
+++ routes/wishList.js
@@ -4,31 +4,43 @@ import { db } from "../db.js";
 
 const router = express.Router();
 
-//Adding a book to the wishlist
+// Adding a book to the wishlist
 router.post("/wishlist/add", async (req, res) => {
-    try {
-        const { title, author, publicationYear, username } = req.body;
+  try {
+    const { title, author, publicationYear, username } = req.body;
 
-        // error retry message if book title, author, or userID does not match
-        if (!title || !author || !username) {
-            res.status(400).json({
-                error: "Title, author or username is missing in the request.",
-            });
-            return;
-        }
-        // inserting the book into a user's wishlist
-        await db
-            .collection("wishlists")
-            .updateOne(
-                { username },
-                { $push: { books: { title, author, publicationYear } } }
-            );
-
-        res.status(200).json({ message: "Book added to the wishlist." });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Error message." });
+    // error retry message if book title, author, or userID does not match
+    if (!title || !author || !username) {
+      res.status(400).json({
+        error: "Title, author or username is missing in the request.",
+      });
+      return;
     }
+
+    // Check if the wishlist exists for the user
+    const wishlist = await db.collection("wishlists").findOne({ username });
+
+    if (!wishlist) {
+      // Wishlist doesn't exist, create a new one
+      await db.collection("wishlists").insertOne({
+        username,
+        books: [{ title, author, publicationYear }],
+      });
+      res.status(200).json({ message: "New wishlist created and book added." });
+    } else {
+      // inserting the book into a user's wishlist
+      await db
+        .collection("wishlists")
+        .updateOne(
+          { username },
+          { $push: { books: { title, author, publicationYear } } }
+        );
+      res.status(200).json({ message: "Book added to the wishlist." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error message." });
+  }
 });
 
 //Removing a book to the wishlist
